21819228a0c88d11d65f83830f86534d78df5d4b
Ashwini Maurya <mauryaashvini99@gmail.com>
2024-10-05 17:21:37 +0530

    changes made in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Import React Router components
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>  {/* BrowserRouter handles routing */}
-      <Routes>
-        <Route path="*" element={<App />} />  {/* '*' wildcard route renders App for all paths */}
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
